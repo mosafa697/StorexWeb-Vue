6c1a31186cd4e3cabe8c91e22c69c802021185ac
Mostafa Magdy <mostafa.magdy@iken.tech>
2023-01-15 16:29:10 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <welcame :posts="posts" />
+    <div>
+      <ul>
+        <li v-if="posts" v-for="(post, index) in posts" :key="index">
+          <span> {{ post.breed }} </span>
+          <span> {{ post.country }} </span>
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  props: {
+    posts: []
+  },
+  methods: {
+    getPosts() {
+      fetch("https://catfact.ninja/breeds")
+        .then((response) => response.json())
+        .then((data) => this.posts = data.data);
+    },
+  },
+  created(){
+    this.getPosts();
+  }
+};
+</script>
